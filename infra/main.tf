f824a62b3412079d1c9ca742b2b0add04f9517a9
Guilherme Garcia <guigarcia.a@outlook.com>
2024-02-23 23:33:36 -0300

    created the first structure of infra

---
+++ infra/main.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  endpoints {
+    lambda = "http://localhost:4566"
+    sns    = "http://localhost:4566"
+    rds    = "http://localhost:4566"
+    iam    = "http://localhost:4566"
+  }
+}
+
+# SQS Queue
+resource "aws_sqs_queue" "process_queue" {
+  name = "process_queue"
+}
+
+# Lambda Function
+resource "aws_lambda_function" "lambda_process" {
+  filename      = "lambda_process.zip"
+  function_name = "lambda_process"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_process.lambda_handler"
+  runtime       = "python3.8"
+}
+
+# Lambda SQS Queue Trigger
+resource "aws_lambda_event_source_mapping" "sqs_trigger" {
+  event_source_arn = aws_sqs_queue.process_queue.arn
+  function_name    = aws_lambda_function.lambda_process.function_name
+  batch_size       = 10
+}
+
+# IAM Role for Lambda
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Principal" : {
+          "Service" : "lambda.amazonaws.com"
+        },
+        "Effect" : "Allow"
+      }
+    ]
+  })
+}
+
+# IAM Policy Attachment for Lambda Role (Access to SQS)
+resource "aws_iam_policy_attachment" "lambda_sqs_access" {
+  name       = "lambda_sqs_access"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+# DynamoDB Table
+resource "aws_dynamodb_table" "tb_process" {
+  name         = "tb_process"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
--- infra/start.sh
+++ infra/start.sh
@@ -1,2 +1,31 @@
-docker-compose -f ./.localstack/localstack.yml up
+# docker-compose -f localstack/localstack.yml up -d;
 
+
+# echo "Starting the application"
+
+# zip -r ./lambda_process.zip ../app/src/*.py
+
+# tflocal init
+# tflocal apply -auto-approve
+
+# echo "Application started"
+
+echo "Sending a message to the queue"
+
+awslocal sqs send-message --queue-url http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/process_queue --message-body "Hello World"
+
+awslocal sqs send-message --queue-url http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/terraform-example-queue.fifo --message-body "Hello World"
+
+# echo "Starting the tests"
+
+# pytest ../app/tests
+
+# echo "Tests finished"
+
+# echo "Shutting down the application"
+
+# tflocal destroy -auto-approve
+
+# docker-compose -f ./localstack/localstack.yml down
+
+# echo "Application shut down"
