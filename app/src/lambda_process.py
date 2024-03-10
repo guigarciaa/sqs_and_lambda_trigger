7f05f9b6f3159187c3dc1f2f76d213960d0bf7ac
Guilherme Garcia <guigarcia.a@outlook.com>
2024-03-10 20:26:57 -0300

    Creating role for lambda access dynamoDB, adding prints for read events.

--- app/src/lambda_process.py
+++ app/src/lambda_process.py
@@ -1,10 +1,29 @@
-def lambda_handler(event, context):
-    print(event, context)
-    
-    # Return a response
-    reponse = {
-        'statusCode': 200,
-        'body': "Hello, World!"
-    }
+import boto3
 
-    return reponse
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('tb_process')
+
+    # Print the event data
+    print(f'Event recevided: {event}')
+
+    try:
+        # Insert a new record
+        table.put_item(
+        Item={
+            'id': range(1, 1000),
+            'name': f'Process {range(1, 1000)}',
+            'status': 'Running'
+        })
+
+        print('Record inserted')
+        return {
+            'statusCode': 200,
+            'body': "Record inserted"
+        }
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': "Error inserting record"
+        }
--- infra/main.tf
+++ infra/main.tf
@@ -4,10 +4,10 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
   endpoints {
-    lambda = "http://localhost:4566"
-    sns    = "http://localhost:4566"
-    rds    = "http://localhost:4566"
-    iam    = "http://localhost:4566"
+    lambda   = "http://localhost:4566"
+    sns      = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+    iam      = "http://localhost:4566"
   }
 }
 
@@ -16,25 +16,25 @@ resource "aws_sqs_queue" "process_queue_dlq" {
   name = "dlq"
 }
 
-
 # SQS Queue
 resource "aws_sqs_queue" "process_queue" {
   name = "process_queue"
 
-   # Configure Redrive Policy to send messages to the DLQ after 3 failed attempts
+  # Configure Redrive Policy to send messages to the DLQ after 3 failed attempts
   redrive_policy = jsonencode({
-    deadLetterTargetArn    = aws_sqs_queue.process_queue_dlq.arn
-    maxReceiveCount        = 3
+    deadLetterTargetArn = aws_sqs_queue.process_queue_dlq.arn
+    maxReceiveCount     = 3
   })
 }
 
 # Lambda Function
 resource "aws_lambda_function" "lambda_core" {
-  filename      = "lambda_process.zip"
-  function_name = "lambda_process"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "lambda_process.lambda_handler"
-  runtime       = "python3.8"
+  filename         = "lambda_process.zip"
+  source_code_hash = filebase64sha256("lambda_process.zip")
+  function_name    = "lambda_process"
+  handler          = "lambda_process.lambda_handler"
+  runtime          = "python3.8"
+  role             = aws_iam_role.lambda_execution_role.arn
 }
 
 # Lambda SQS Queue Trigger
@@ -45,28 +45,51 @@ resource "aws_lambda_event_source_mapping" "sqs_trigger" {
 }
 
 # IAM Role for Lambda
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_role"
-
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "lambda_execution_role"
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
       {
-        "Action" : "sts:AssumeRole",
+        "Effect" : "Allow",
         "Principal" : {
           "Service" : "lambda.amazonaws.com"
         },
-        "Effect" : "Allow"
+        "Action" : "sts:AssumeRole"
       }
     ]
   })
 }
 
-# IAM Policy Attachment for Lambda Role (Access to SQS)
-resource "aws_iam_policy_attachment" "lambda_sqs_access" {
-  name       = "lambda_sqs_access"
-  roles      = [aws_iam_role.lambda_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+resource "aws_iam_policy_attachment" "lambda_execution_policy_attachment" {
+  name       = "lambda_execution_policy_attachment"
+  roles      = [aws_iam_role.lambda_execution_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy" "dynamodb_access_policy" {
+  name = "dynamodb_access_policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem"
+        ],
+        "Resource" : "arn:aws:dynamodb:us-east-1:000000000000:table/tb_process"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "dynamodb_policy_attachment" {
+  name       = "dynamodb_policy_attachment"
+  roles      = [aws_iam_role.lambda_execution_role.name]
+  policy_arn = aws_iam_policy.dynamodb_access_policy.arn
 }
 
 # DynamoDB Table
